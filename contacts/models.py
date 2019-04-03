2337062ddc860bdcb0de7500766497173a9cea39
oriefy <shahzaibakash@gmail.com>
2019-04-03 15:27:06 +0500

    add str method for contact model

--- contacts/models.py
+++ contacts/models.py
@@ -9,3 +9,6 @@ class Contact(models.Model):
     company = models.CharField(max_length=50)
     website = models.CharField(max_length=50, blank=False)
     
+
+    def __str__(self):
+        return self.name
