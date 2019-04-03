2a31675ec9a665f4de9697b5798ba6bc2e890f0e
oriefy <shahzaibakash@gmail.com>
2019-04-03 13:58:16 +0500

    add accounts model

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from contacts.models import Contact
+
+
+PRECISION = getattr(settings, 'LEDGER_PRECISION', {'max_digits':12, 'decimal_places':2})
 
 # Create your models here.
+class Account(models.Model):
+    """
+    The ledger account linked with contacts
+    """
+    person = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    balance = models.IntegerField(default=0)
+
