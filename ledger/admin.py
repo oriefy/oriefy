f488b64b1a2d7263add3f5bdd1149c6612e17629
oriefy <shahzaibakash@gmail.com>
2019-04-03 15:05:44 +0500

    add Admin views for Account and Transaction model

--- ledger/admin.py
+++ ledger/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from ledger.models import Account, Transaction
 
 # Register your models here.
-admin.site.register(Account)
-admin.site.register(Transaction)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    '''Admin View for Account'''
+
+    list_display = ('person', 'balance')
+
+    raw_id_fields = ('person',)
+    search_fields = ('person__name',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    '''Admin View for Transaction'''
+
+    list_display = (
+        'account',
+        'amount',
+        'transaction_type',
+        )
+    list_filter = ('transaction_type', 'account')
+    raw_id_fields = ('account',)
+    search_fields = ('account', 'description')
+    date_hierarchy = 'created_at'
+    ordering = ('created_at',)
