ed974daa4d24c8a4484854f66873877f784b634b
oriefy <shahzaibakash@gmail.com>
2019-04-05 15:19:47 +0500

    create export_to_csv mixin

---
+++ oriefy/utils.py
@@ -0,0 +1,25 @@
+import csv
+from django.http import HttpResponse
+
+
+class ExportCsvMixin:
+    """
+    A Django Admin Mixin that allows the user to extract the selected rows as csv record
+    """
+
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
